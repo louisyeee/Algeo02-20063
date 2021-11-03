09de23efebd1983e5300958de9154ef63bfb24fe
louisyeee <13520063@std.stei.itb.ac.id>
2021-11-03 21:12:01 +0700

    Menambahkan fungsi untuk memproses hasil SVD hingga menghasilkan image hasil kompres

--- src/backend/svd.py
+++ src/backend/svd.py
@@ -1,5 +1,6 @@
 import numpy
 from PIL import Image
+from matrix import *
 
 
 def openImage(imagePath):  # Fungsi untuk membuka file image dan menghasilkan matriks RGB
@@ -10,6 +11,15 @@ def openImage(imagePath):  # Fungsi untuk membuka file image dan menghasilkan ma
     matrixBlue = image[:, :, 2]
 
     return [matrixRed, matrixGreen, matrixBlue, originalImage]
+
+
+def createColorMatrix(m1, m2, m3, limitSVD):
+    m1 = m1[:, :limitSVD]
+    m2 = m2[:limitSVD, :limitSVD]
+    m3 = m3[:limitSVD, :]
+    m2 = np.dot(m1, m2)
+    m3 = np.dot(m2, m3)
+    return m3
 
 
 imagePath = input("Masukkan nama file: ")
@@ -26,3 +36,23 @@ ratio = int(input("Masukkan ratio pengurangan: "))
 
 # Menentukan limit SVD
 limitSVD = round(oriImageSize * ratio / 100 / (1+oriWidth+oriHeight) / 3)
+
+# Menghasilkan matriks U, S, V hasil SVD
+URed, SRed, VRed = svd(matrixRed)
+UGreen, SGreen, VGreen = svd(matrixGreen)
+UBlue, SBlue, VBlue = svd(matrixBlue)
+
+# Menghasilkan matriks warna setelah SVD sesuai rasio
+finalRed = createColorMatrix(URed, SRed, VRed)
+finalGreen = createColorMatrix(UGreen, SGreen, VGreen)
+finalBlue = createColorMatrix(UBlue, SBlue, VBlue)
+
+# Menghasilkan gambar sesuai RGB
+imageRed = Image.fromarray(finalRed, mode=None)
+imageGreen = Image.fromarray(finalGreen, mode=None)
+imageBlue = Image.fromarray(finalBlue, mode=None)
+
+# Menghasilkan image setelah kompres
+finalImage = Image.merge("RGB", (imageRed, imageGreen, imageBlue))
+
+finalImage.show()
