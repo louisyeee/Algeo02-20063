04f58ca047f5d146088b1f11d8430f34fbdf8596
weslygio <16520363@mahasiswa.itb.ac.id>
2021-11-06 15:21:13 +0700

    svd dengan qr algorithm

--- src/backend/matrix.py
+++ src/backend/matrix.py
@@ -1,79 +1,96 @@
 import numpy as np
-import sympy as sym
-
-epsilon = 0.000000001
+# import sympy as sym
 
 
-def eigenmatrix(A: sym.Matrix, egv: sym.Float) -> sym.Matrix:
-    row = A.rows
-    I = sym.eye(row)
-    Z = egv * I - A
-    return Z
-
-
-# Menghasilkan array of eigenvalue turut mengecil
-def eigenvalue_list(A: sym.Matrix) -> np.array:
-    egv = sym.Symbol('egv')    # Eigenvalue
-    row = A.rows
-    I = sym.eye(row)
-    Z = egv * I - A
-    char_eq = sym.det(Z)
-    soln = np.array(sym.solve(char_eq, egv))
-
-    try:
-        soln = np.sort(soln)[::-1]
-    except:
-        for i in range(len(soln)):
-            soln[i] = sym.re(sym.N(soln[i]))
-        soln = np.sort(soln)[::-1]
-
-    return soln
-
-
-# Mendekomposisi matriks A menjadi tuple of (U,S,V)
+# Mendekomposisi matriks A menjadi tuple of (U,S,V^t)
+# dengan A = U . S . V^t
 def svd(A: np.matrix) -> (np.matrix, np.matrix, np.matrix):
-    A = sym.Matrix(A)
+    AT = np.transpose(A)
+    AAT = np.matrix(np.matmul(A, AT))
+    ATA = np.matrix(np.matmul(AT, A))
 
-    V = np.matrix(singular_matrix(A.T * A))
-    U = np.matrix(singular_matrix(A * A.T))
+    egv_list, V = eigen(ATA)
+    egv_list, U = eigen(AAT)
 
-    ''' Matriks Sigma '''
     S = np.matrix(np.zeros(A.shape))
-    egv_list = eigenvalue_list(A.T * A)
     for i in range(len(egv_list)):
         egv = egv_list[i]
-        if egv > 0:
-            S[i,i] = sym.N(sym.sqrt(egv))
-        else:
+        S[i, i] = np.sqrt(egv)
+
+    for i in range(len(egv_list)):
+        U[:,i] = np.dot(A, V[:,i])/S[i,i]
+
+    Vt = np.transpose(V)
+
+    return U, S, Vt
+
+
+# Menghasilkan (eigenvalues, eigenvectors)
+# dengan QR algorithm
+def eigen(A: np.matrix) -> (np.array, np.matrix):
+    accuracy_level = 10
+    K = np.identity(A.shape[0])
+
+    for i in range(accuracy_level):
+        Q, R = np.linalg.qr(A)
+        K = np.matmul(K, Q)
+        A = np.matmul(R, Q)
+
+    soln = [A[i,i] for i in range(len(A))]
+
+    for i in range(len(soln)):
+        if soln[i] < 0.00001:
+            soln = soln[:i]
             break
 
-    return U, S, V
+    return soln, np.matrix(K)
 
+
+''' -- Algoritma memiliki kompleksitas tinggi -- '''
+
+'''
+def eigenvalue_list(A: sym.Matrix) -> np.array:
+    # egv = sym.Symbol('egv')    # Eigenvalue
+    # row = A.rows
+    # I = sym.eye(row)
+    # Z = egv * I - A
+
+    # char_eq = sym.det(Z)
+    # soln = np.array(sym.solve(char_eq, egv))
+
+    # for i in range(len(soln)):
+    #     soln[i] = sym.re(sym.N(soln[i]))
+    # soln = np.sort(soln)[::-1]
+
+    # return soln
 
 # Matriks singular
-def singular_matrix(A: sym.Matrix) -> sym.Matrix:
+def singular_matrix(A: np.matrix, egv_list: np.array) -> np.matrix:
+    epsilon = 0.000001
     basis_list = []
-    egv_list = eigenvalue_list(A)
-
+    
     for egv in egv_list:
         Z = eigenmatrix(A, egv)
         Z = rref(Z)
+        print('-----')
+        print(Z)
         Z = delete_zero_row(Z)
+        row, col = Z.shape
 
         soln = []
-        paramlist = [i for i in range(Z.cols)]  # list of index of parameters
+        paramlist = [i for i in range(col)]  # list of index of parameters
         sublist = []    # list of substitution tuples
 
         # Setup variable list [x1, x2, x3, ...]
-        for j in range(Z.cols):
+        for j in range(col):
             xn = sym.Symbol(f"x{j+1}")
             soln.append(xn)
 
         # soln = [-x2+x3, x2, x3, ...]
         # paramlist = [1,2]
-        for i in range(Z.rows):
+        for i in range(row):
             foundOne = False
-            for j in range(Z.cols):
+            for j in range(col):
                 if not foundOne and abs(Z[i, j] - 1) < epsilon:
                     foundOne = True
                     indexOne = j
@@ -113,31 +130,47 @@ def singular_matrix(A: sym.Matrix) -> sym.Matrix:
 
             basis_list.append(soln_instance)
 
-    return sym.Matrix(basis_list).T
+    if len(basis_list) != A.shape[0]:
+        number_of_pads = A.shape[0] - len(basis_list)
+        for i in range(number_of_pads):
+            basis_list = np.r_[basis_list, np.zeros((1, A.shape[0]))]
+
+    return np.matrix(np.transpose(basis_list))
 
 
-# Prekondisi: Matrix A dalam bentuk rref
-def delete_zero_row(A: sym.Matrix) -> sym.Matrix:
-    i = A.rows - 1
-    deleting = True
-    while i >= 0 and deleting:
-        for x in A.row(i):
-            if x != 0:
-                deleting = False
-        if deleting:
-            A.row_del(i)
-            i -= 1
-    return A
+def eigenmatrix(A: np.matrix, egv: float) -> np.matrix:
+    row, col = A.shape
+    I = np.identity(row)
+    Z = egv * I - A
+    return Z
+
+
+# Menghasilkan array of eigenvalue > 0 turut mengecil
+def eigenvalue_list(A: np.matrix) -> np.array:
+    # QR Algorithm
+    accuracy_level = 50
+    for i in range(accuracy_level):
+        Q, R = np.linalg.qr(A)
+        A = np.matmul(R, Q)
+
+    soln = [A[i,i] for i in range(len(A))]
+
+    for i in range(len(soln)):
+        if soln[i] < 0.001:
+            break
+
+    return soln[:i]
 
 
 # Tidak menggunakan sympy.rref() dengan tujuan
 # modifikasi ketelitian saat melakukan OBE
-def rref(A: sym.Matrix) -> sym.Matrix:
-    return sym.Matrix(row_echelon_top(row_echelon_bottom(np.matrix(A, 'float'))))
+def rref(A: np.matrix) -> np.matrix:
+    return row_echelon_top(row_echelon_bottom(A))
 
 
 # Membentuk matriks eselon baris
 def row_echelon_bottom(A: np.matrix) -> np.matrix:
+    epsilon = 0.001
     A = np.array(A, 'float')
     row, col = A.shape
 
@@ -187,6 +220,7 @@ def row_echelon_bottom(A: np.matrix) -> np.matrix:
 # Menghasilkan matriks eselon baris tereduksi dari
 # matriks eselon baris
 def row_echelon_top(A: np.matrix) -> np.matrix:
+    epsilon = 0.001
     A = np.array(A, 'float')
     row, col = A.shape
 
@@ -229,3 +263,34 @@ def row_echelon_top(A: np.matrix) -> np.matrix:
             #    0 0 1 3  A[-1:]
             B = np.array(row_echelon_top(A[:-1,:]))
             return np.matrix(np.vstack([B, A[-1:]]))
+
+
+# Prekondisi: Matrix A dalam bentuk rref
+def delete_zero_row(A: np.matrix) -> np.matrix:
+    deleting = True
+    row, col = A.shape
+    while A.shape[0] > 0 and deleting:
+        deleting = all(A[-1,j] == 0 for j in range(col))
+        if deleting:
+            A = A[:-1]
+            col -= 1
+
+    return A
+
+# Menghasilkan array of eigenvalue > 0 turut mengecil
+def eigenvalue_list(A: np.matrix) -> np.array:
+    # QR Algorithm
+    accuracy_level = 50
+    for i in range(accuracy_level):
+        Q, R = np.linalg.qr(A)
+        A = np.matmul(R, Q)
+
+    soln = [A[i,i] for i in range(len(A))]
+
+    for i in range(len(soln)):
+        if soln[i] < 0.001:
+            break
+
+    return soln[:i]
+
+'''
