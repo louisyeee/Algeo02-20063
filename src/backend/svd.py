d12d1b56c385dd05adf2284597fe861f350012d2
louisyeee <13520063@std.stei.itb.ac.id>
2021-11-09 10:18:56 +0700

    Membuat fungsi untuk testing hasil svd

--- src/backend/svd.py
+++ src/backend/svd.py
@@ -14,17 +14,18 @@ def openImage(imagePath):  # Fungsi untuk membuka file image dan menghasilkan ma
 
 
 def createColorMatrix(m1, m2, m3, limitSVD):
-    m1 = m1[:, :limitSVD]
-    m2 = m2[:limitSVD, :limitSVD]
-    m3 = m3[:limitSVD, :]
-    m2 = np.dot(m1, m2)
-    m3 = np.dot(m2, m3)
+    #     m1 = m1[:, :limitSVD]
+    #     m2 = m2[:limitSVD, :limitSVD]
+    #     m3 = m3[:limitSVD, :]
+    m2 = np.matmul(m1, m2)
+    m3 = np.matmul(m2, m3)
+    m3 = m3.astype('uint8')
     return m3
 
 
-imagePath = input("Masukkan nama file: ")
-matrixRed, matrixGreen, matrixBlue, originalImage = openImage()
-
+# imagePath = input("Masukkan nama file: ")
+matrixRed, matrixGreen, matrixBlue, originalImage = openImage("lena.png")
+originalImage.show()
 # Menghasilkan nilai lebar dan tinggi dari Image
 oriWidth, oriHeight = originalImage.size
 
@@ -32,20 +33,21 @@ oriWidth, oriHeight = originalImage.size
 oriImageSize = oriWidth * oriHeight * 3
 
 # Menerima rasio pengurangan ukuran yang diinginkan user
-ratio = int(input("Masukkan ratio pengurangan: "))
+ratio = float(input("Masukkan ratio pengurangan: "))
+ratio = 100-ratio
 
 # Menentukan limit SVD
 limitSVD = round(oriImageSize * ratio / 100 / (1+oriWidth+oriHeight) / 3)
-
+print(limitSVD)
 # Menghasilkan matriks U, S, V hasil SVD
 URed, SRed, VRed = svd(matrixRed)
 UGreen, SGreen, VGreen = svd(matrixGreen)
 UBlue, SBlue, VBlue = svd(matrixBlue)
 
 # Menghasilkan matriks warna setelah SVD sesuai rasio
-finalRed = createColorMatrix(URed, SRed, VRed)
-finalGreen = createColorMatrix(UGreen, SGreen, VGreen)
-finalBlue = createColorMatrix(UBlue, SBlue, VBlue)
+finalRed = createColorMatrix(URed, SRed, VRed, limitSVD)
+finalGreen = createColorMatrix(UGreen, SGreen, VGreen, limitSVD)
+finalBlue = createColorMatrix(UBlue, SBlue, VBlue, limitSVD)
 
 # Menghasilkan gambar sesuai RGB
 imageRed = Image.fromarray(finalRed, mode=None)
