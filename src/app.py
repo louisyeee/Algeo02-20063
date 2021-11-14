858bd8e8e182513985cad5dddd42fd6147f64a89
Claudia <88203931+clauculus@users.noreply.github.com>
2021-11-14 15:55:55 +0700

    rapiin directory, fitur lengkap

--- src/app.py
+++ src/app.py
@@ -1,6 +1,61 @@
 from PIL import Image
 from image_process import *
 from flask import Flask
+from flask import render_template, request, redirect, url_for, send_file
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'static/'
+
+app.secret_key = "tubesalgelo"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+
+def index():
+    return render_template('index.html')
+
+@app.route('/compress',methods = ['POST'])
+
+def compress_app():
+    if request.method == 'POST':
+        percen = request.form
+        perc = float(percen['rate'])
+
+        if 'file' in request.files:
+
+            pic = request.files['file']
+            # pic.save(secure_filename(pic.filename))
+            filename = secure_filename(pic.filename)
+            pic.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+
+            pic = Image.open(request.files['file'].stream)
+            finalImage, diffRatio, runtime = compress(pic,perc) 
+
+            global finalname
+            finalname = 'compressed_' + filename
+            finalImage = finalImage.save(os.path.join(app.config['UPLOAD_FOLDER'],finalname))
+
+            
+        return render_template('index.html', rtime = runtime, pixel = diffRatio, imagename = filename, imagefinal = finalname )
+
+    return redirect('/')
+
+@app.route('/download')
+def download():
+    global finalname
+    path = UPLOAD_FOLDER + finalname
+    return send_file(path, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+"""
+from PIL import Image
+from image_process import *
+from flask import Flask
 from flask import render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
@@ -62,3 +117,4 @@ def compress_app():
 if __name__ == '__main__':
     app.run(debug=True)
 
+"""
