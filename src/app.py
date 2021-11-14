9f777d1c2b9ab3c299868d19bab64b1679286ed2
Claudia <88203931+clauculus@users.noreply.github.com>
2021-11-14 21:47:13 +0700

    finalize

--- src/app.py
+++ src/app.py
@@ -1,48 +1,60 @@
 from PIL import Image
 from image_process import *
 from flask import Flask
-from flask import render_template, request, redirect, url_for, send_file
+from flask import render_template, request, redirect, url_for, send_file, flash
 from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'static/'
+UPLOAD_FOLDER = 'static/' # directory for uploading image
+ALLOWED_EXTENSIONS = {'jpg','jpeg','png'} # web can only process this extension
 
-app.secret_key = "tubesalgelo"
+app.secret_key = "tubesalgeotercinta"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
+def allowed_file(filename): # checking file extension
+    return '.' in filename and \
+        filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
 
+# Main page
+@app.route('/')
 def index():
     return render_template('index.html')
 
+# If the submit button is pressed
 @app.route('/compress',methods = ['POST'])
-
 def compress_app():
     if request.method == 'POST':
-        percen = request.form
-        perc = float(percen['rate'])
-
-        if 'file' in request.files:
+        # Check whether the request has rate and file 
+        if 'rate' not in request.form or 'file' not in request.files:
+            flash('There is no input')
+            return redirect('/') #
+        else:
+            percen = request.form
+            perc = float(percen['rate'])
 
             pic = request.files['file']
-            # pic.save(secure_filename(pic.filename))
-            filename = secure_filename(pic.filename)
-            pic.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
 
-            pic = Image.open(request.files['file'].stream)
-            finalImage, diffRatio, runtime = compress(pic,perc) 
+            if (perc<0) or (perc>=100) or (not allowed_file(pic.filename)) or (pic.filename == ''): # validating user input
+                flash('Wrong input, please retry')        
+                return redirect('/')    
+            else:
+                filename = secure_filename(pic.filename) # take the name of the pic
+                pic.save(os.path.join(app.config['UPLOAD_FOLDER'],filename)) # save to static folder
 
-            global finalname
-            finalname = 'compressed_' + filename
-            finalImage = finalImage.save(os.path.join(app.config['UPLOAD_FOLDER'],finalname))
+                pic = Image.open(request.files['file'].stream) # convert into PIL Image
+                finalImage, diffRatio, runtime = compress(pic,perc) # compression
 
-            
-        return render_template('index.html', rtime = runtime, pixel = diffRatio, imagename = filename, imagefinal = finalname )
+                global finalname
+                finalname = 'compressed_' + filename
+                finalImage = finalImage.save(os.path.join(app.config['UPLOAD_FOLDER'],finalname)) # save the compressed picture to static folder
+
+                return render_template('index.html', rtime = runtime, pixel = diffRatio, imagename = filename, imagefinal = finalname )
 
     return redirect('/')
 
+# To download the compressed picture
 @app.route('/download')
 def download():
     global finalname
@@ -51,70 +63,3 @@ def download():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-"""
-from PIL import Image
-from image_process import *
-from flask import Flask
-from flask import render_template, request, redirect, url_for
-from werkzeug.utils import secure_filename
-import os
-import base64
-from io import BytesIO
-
-
-def pil2datauri(img):
-    #converts PIL image to datauri
-    data = BytesIO()
-    img.save(data, "JPEG")
-    data64 = base64.b64encode(data.getvalue())
-    return u'data:img/jpeg;base64,'+data64.decode('utf-8')
-
-
-app = Flask(__name__)
-
-UPLOAD_FOLDER = 'static/'
-
-app.secret_key = "tubesalgelo"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/compress',methods = ['POST'])
-def compress_app():
-    if request.method == 'POST':
-        percen = request.form
-        perc = float(percen['rate'])
-
-        if 'file' in request.files:
-
-            pic = request.files['file']
-            # pic.save(secure_filename(pic.filename))
-            filename = secure_filename(pic.filename)
-            pic.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-
-            pic = Image.open(request.files['file'].stream)
-            #pic = Image.Image(pic)
-            finalImage, diffRatio, runtime = compress(pic,perc)
-
-            finalname = 'compressed.jpg'
-            finalImage = finalImage.save(os.path.join(app.config['UPLOAD_FOLDER'],finalname))
-
-            #finalimg = pil2datauri(finalImage)
-            
-            #finalname = secure_filename(f'{filename}_compress')
-            # finalimg.save(os.path.join(app.config['UPLOAD_FOLDER'],finalname))
-            
-        return render_template('index.html', rtime = runtime, pixel = diffRatio, imagename = filename, imagefinal = finalname )
-
-    return redirect('/')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-"""
